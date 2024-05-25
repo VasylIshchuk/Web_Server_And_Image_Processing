56737edbfb5f300e945a3e03eb90d9fd27e7d0b8
VasylIshchuk <Vasyl.Ishchuk@live.umcs.edu.pl>
2024-05-25 19:27:53 +0200

    Added methods for adding, displaying a list of Rectangle objects, and generating SVG code in the controller

--- src/main/java/org/umcs/imagewebserver/RectangleController.java
+++ src/main/java/org/umcs/imagewebserver/RectangleController.java
@@ -1,8 +1,11 @@
 package org.umcs.imagewebserver;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 @RestController
 //This annotation indicates that the class is a RESTful web service CONTROLLER and contains a method to
@@ -12,12 +15,37 @@ import org.springframework.web.bind.annotation.RestController;
 // Define the base URL by which the controller will handle requests
 public class RectangleController {
 
-    private final Rectangle  rectangle = new Rectangle(40,20,70,30,"purple");
-
+    private final Rectangle  rectangleDefault = new Rectangle(40,20,70,30,"purple");
+    private List<Rectangle> rectangles = new ArrayList<>();
+    
     @GetMapping("get")
 //Marks a method as HTTP GET request handle.This method will be called for GET requests by URL ("/rectangle/get")
     public Rectangle getRectangle(){
-        return rectangle;
+        return rectangleDefault;
     }
-
+    @PostMapping("add")
+//    This annotation indicates, that method will be called only for HTTP POST-requests
+//    on pointed URL-way, which in this case ("/rectangle/add").
+    public String addRectangle(){
+        rectangles.add(rectangleDefault);
+        return "Successfully added";
+    }
+    @GetMapping("getList")
+    public List<Rectangle> getRectangleList(){
+        return rectangles;
+    }
+    @GetMapping("getSVG")
+    public StringBuffer getSvgRectangles(){
+        StringBuffer svgRrectangles = new StringBuffer();
+        svgRrectangles.append("<svg width=\"300\" height=\"130\" xmlns=\"http://www.w3.org/2000/svg\">\n");
+        for(Rectangle rectangle : rectangles){
+            svgRrectangles.append(String.format(Locale.ENGLISH,
+                    "\t<rect width=\"%d\" height=\"%d\" x=\"%d\" y=\"%d\" fill=\"%s\" />\n"
+                    ,rectangle.getWidth(),rectangle.getHeight()
+                    ,rectangle.getX(),rectangle.getY()
+                    ,rectangle.getColor()));
+        }
+        svgRrectangles.append("</svg>\n\n");
+        return svgRrectangles;
+    }
 }
